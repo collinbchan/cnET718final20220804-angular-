7ada6c0e9784f13197ccf49c12b845d1f476bb3e
collinbchan <bchancollin@gmail.com>
2022-08-04 09:42:48 -0400

    2 files modified, 4 added

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+
+export class ProductAlertsComponent {
+
+  @Input() product!: Product;
+
+}
